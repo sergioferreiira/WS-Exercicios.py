8651a9d2bacc87bac5a90a5f7923aa3d28b57835
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-01-17 21:21:43 -0300

    Ultimo desafio enviado pelo professor do curso da udemy python

--- exercicio_131.py
+++ exercicio_131.py
@@ -13,7 +13,7 @@ Requisitos:
 """
 lista_de_listas_de_inteiros = [
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [9, 1, 8, 7, 9, 7, 2, 1, 6, 8],
     [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
     [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
     [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
@@ -25,3 +25,22 @@ lista_de_listas_de_inteiros = [
     [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
     [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
 ]
+
+def encontraPrimeiroRepetidor (lista):
+    newlist = []
+    print("------------------------------")
+    print(lista)
+    for numero in lista:
+        if numero in newlist:
+            print(f"repetidor: {numero} ")
+            break
+        else:
+            newlist.append(numero)
+
+          
+    
+
+for lista in lista_de_listas_de_inteiros:
+    encontraPrimeiroRepetidor(lista)
+
+
