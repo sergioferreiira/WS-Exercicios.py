c6de5ecb7f16276d4694890a82b290251b91d652
Sergio Ferreira <88517724+sergioferreiira@users.noreply.github.com>
2024-01-18 20:54:39 -0300

    Atualização com correção do professor

--- exercicio_131.py
+++ exercicio_131.py
@@ -11,6 +11,45 @@ Requisitos:
         [1, 4, 9, 8, ->9<-, 4, 8] (retorne 9)
     Se não encontrar duplicados na lista, retorne -1
 """
+
+# ######### MINHA SOLUÇÃO \/ ####################
+
+# lista_de_listas_de_inteiros = [
+#     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+#     [9, 1, 8, 7, 9, 7, 2, 1, 6, 8],
+#     [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+#     [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+#     [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+#     [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+#     [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+#     [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+#     [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+#     [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+#     [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+#     [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+# ]
+
+# def encontraPrimeiroRepetidor (lista):
+#     newlist = []
+#     print("------------------------------")
+#     print(lista)
+#     for numero in lista:
+#         if numero in newlist:
+#             print(f"repetidor: {numero} ")
+#             break
+#         else:
+#             newlist.append(numero)
+
+          
+    
+
+# for lista in lista_de_listas_de_inteiros:
+#     encontraPrimeiroRepetidor(lista)
+
+
+
+# ################## SOLUÇÃO PROFESSOR ##################
+
 lista_de_listas_de_inteiros = [
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     [9, 1, 8, 7, 9, 7, 2, 1, 6, 8],
@@ -27,20 +66,27 @@ lista_de_listas_de_inteiros = [
 ]
 
 def encontraPrimeiroRepetidor (lista):
-    newlist = []
-    print("------------------------------")
-    print(lista)
+
+    lista_checada = set()
+    valor_numero = -1
+
     for numero in lista:
-        if numero in newlist:
-            print(f"repetidor: {numero} ")
+        if numero in lista_checada:
+            valor_numero = numero
             break
         else:
-            newlist.append(numero)
+            lista_checada.add(numero)
+
+    return valor_numero
+
+            
+
 
           
     
 
 for lista in lista_de_listas_de_inteiros:
-    encontraPrimeiroRepetidor(lista)
+    print(encontraPrimeiroRepetidor(lista))
 
 
+# 
