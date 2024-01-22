6bde441af482def237a733e47d83fec70a7a51d1
Sergio Ferreira <88517724+sergioferreiira@users.noreply.github.com>
2024-01-22 07:51:19 -0300

    Add files via upload
    Atualização de estudos

---
+++ dictcomprehension.py
@@ -0,0 +1,43 @@
+# Dictionary Comprehension e Set Comprehension
+produto = {
+    'nome': 'Caneta Azul',
+    'preco': 2.5,
+    'categoria': 'Escritório',
+}
+
+dc = {
+    chave: valor.upper()
+    if isinstance(valor, str) else valor
+    for chave, valor
+    in produto.items()
+    if chave != 'categoria'
+}
+
+lista = [
+    ('a', 'valor a'),
+    ('b', 'valor a'),
+    ('b', 'valor a'),
+]
+dc = {
+    chave: valor
+    for chave, valor in lista
+}
+
+s1 = {2 ** i for i in range(10)}
+print(s1)
+
+
+
+lista = [
+    ('a', 'valor 1'),
+    ('b', 'valor 2'),
+    ('b', 'valor 3'),
+    ('b', 'valor 4'),
+    ('b', 'valor 5'),
+    ('b', 'valor 6'),
+]
+
+
+
+print(dict(lista))
+print(lista)
