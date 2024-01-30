5c7a9849882941e11ec666ca4bcee262b954edcd
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-01-30 07:01:55 -0300

    atualização sobre closure e utilização de decoradores @função

--- func_decoradoras.py
+++ func_decoradoras.py
@@ -4,16 +4,24 @@
 # Decoradores são usados para fazer o Python
 # usar as funções decoradoras em outras funções.
 
-
+# aqui temos um classico closure que recebemos uma função
 def cria_func(func):
+    # guardada a informação da função recebemos agora os valores dos parametros no caso utilizando *args = argumentos não nomeados
     def interna(*args):
+        # para cada argumento não nomeados realizamos a restrição vara verificar se o parametro isistance=(param , str)
         for arg in args:
             is_string(arg)
+        # no resultado guardamos agora o valor da função passada para a closure e passamos também seu argumento já tratado para dentro da mesma
         resultado = func(*args)
+        # retornamos o resultado na def interna(*args)
         return resultado
+    # retornamos da dec cria_func(func) o valor da def interna como resultado final da função
     return interna
-        
 
+# a função @cria_func e o sintax sugar utilizando ela não é necessario alterar o codigo na parte de formatar variaveis e trocar
+# ordem de puxar as funções pois ao utilizar o sintaxnsugar  estamos automaticamente fazendo com que o invert_string 
+# realize a execução primeiramente da cria_func que é onde será tratado os dados antes de gerar a saida
+@cria_func
 def invert_string(string):
     return string[::-1]
 
@@ -22,6 +30,25 @@ def is_string(arg):
         raise TypeError("Deve ser digitado uma string")
 
 
-verifica_se_e_string = cria_func(invert_string)
-user_input = verifica_se_e_string(123)
+user_input = invert_string('123')
 print(user_input)
+
+
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+
+def criadora_de_funcao(func):
+    def tratadora_de_param(*args, **kwargs):
+        resultado = func(*args, **kwargs)
+        return resultado
+    return tratadora_de_param
+
+@criadora_de_funcao
+def soma(x ,y):
+    return x + y
+
+
+
+somar = soma(2,2)
+print(somar)
