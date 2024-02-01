df06534d56834da1d4ec3e0a7162729fd8658444
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-02-01 07:08:20 -0300

    atualização no arquivo hackerank, treino de logica

--- hackerrank.py
+++ hackerrank.py
@@ -2,9 +2,18 @@ from itertools import product
 
 
 
-A = []
-B = [] 
+user_input1 = input("digite o valor a: ")
+user_input2 =  input("digite o valor b: ")
 
-c = product(A , B)
+def recebedor_de_lista(*lista1):
+    def unidor_de_lista(*lista2):
+        resultado = list(product(lista1 , lista2))
+        return resultado
+    return unidor_de_lista
 
-print(*c)
+
+
+unindo = recebedor_de_lista(user_input1)
+uniu = unindo(user_input2)
+
+print(uniu)
